94a7e787c63a6c465acd04d9b6ea921967c3287a
riccardo-ottavi <ottavi.ps@libero.it>
2025-11-26 15:49:51 +0100

    inizializza server

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 //numero porta
 const port = 3000;
 
+// Route di test
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is running' });
+});
+
 //avvia il server
 app.listen(port, () => {
     console.log(`Game server listening on port ${port}`)
